aac68aefca1c0e29a0a347c3bc22431bcaa014c9
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2025-03-13 17:29:21 -0300

    initial code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,14 +14,18 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            Example of Integrate Log in with PayPal with Identity API using
+            NextJS
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
               src/app/page.tsx
             </code>
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Create your account in PayPal developer
+          </li>
+          <li className="tracking-[-.01em]">
+            Add your credentials for testing just like the .env.example
           </li>
         </ol>
 
@@ -40,14 +44,6 @@ export default function Home() {
               height={20}
             />
             Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
           </a>
         </div>
       </main>
